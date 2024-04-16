609d9f25527128492389d3ad8ad169b298406299
Vũ Thị Linh <vuthilinh.jvb@gmail.com>
2024-04-16 14:46:10 +0700

    fix: all file

--- predict.py
+++ predict.py
@@ -1,31 +1,21 @@
 from tensorflow.train import latest_checkpoint
 from tensorflow.keras.models import load_model
 from create_model import create_model
-from utils.config import MODEL_PATH
 from transformers import TFAutoModel
 from utils.tokenizer import PRETRAINED_MODEL
 from utils.variables import df_test, tokenizer
 from utils.preprocess_text import preprocess
 import numpy as np
-import pandas as pd
 from tensorflow.data import Dataset
-
+from utils.preprocess_user_data import preprocess_data
+import pandas as pd
 import urllib.request
 import os
 import streamlit as st
-
-#todo
-from utils.preprocess_user_data import preprocess_data
-
-# pretrained_bert = TFAutoModel.from_pretrained(PRETRAINED_MODEL, output_hidden_states=True)
-# reloaded_model = create_model(pretrained_bert)
-# # reloaded_model.load_weights("F:/jvb/Sentiment-Analysis-leanhtu2/Sentiment-Analysis-leanhtu/model/4_12_2.h5")
-# reloaded_model.load_weights(f"{MODEL_PATH}/4_12_2.h5")
-
-
+@st.cache_resource
 def load_model():
     if not os.path.isfile('model.h5'):
-        urllib.request.urlretrieve('https://github.com/vuthilinhbeng/test_vtwo/blob/main/model/4_12_2.h5', 'model/model.h5')
+        urllib.request.urlretrieve('https://github.com/leanhtu-AI/Sentiment-Analysis/raw/main/model/best.h5', 'model/model.h5')
         pretrained_bert = TFAutoModel.from_pretrained(PRETRAINED_MODEL, output_hidden_states=True)
         reloaded_model = create_model(pretrained_bert)
         reloaded_model.load_weights('model/model.h5')
@@ -34,10 +24,8 @@ def load_model():
 reloaded_model = load_model()
 reloaded_model.summary()
 
-
 replacements = {0: None, 3: 'positive', 1: 'negative', 2: 'neutral'}
 categories = df_test.columns[1:]
-
 
 def print_acsa_pred(replacements, categories, sentence_pred, confidence_scores):
     sentiments = map(lambda x: replacements[x], sentence_pred)
@@ -80,8 +68,6 @@ def show_predict_text(text):
         else:
             results.append(absa_pred)
     return results
-    
-
 
 def predict_csv(model, df):
     input_sentences = df.iloc[:, 0].tolist()
@@ -90,7 +76,7 @@ def predict_csv(model, df):
     pred, confidences = predict_text(model, Dataset.from_tensor_slices(features))
     return pred, confidences
 
-def show_predict_csv(df_clean, output_csv_path):
+def process_predict_csv(df_clean, output_csv_path):
     pred, confidences = predict_csv(reloaded_model, df_clean)
     results = []
     for i in range(len(pred)):
@@ -101,15 +87,15 @@ def show_predict_csv(df_clean, output_csv_path):
                 positive_value = parts[1]
                 confidences_value = float(parts[2])
                 if (positive_value == 'positive' and confidences_value >= 0.6):
-                    parts.append('⭐️⭐️⭐️⭐️⭐️')
+                    parts.append('5')
                 if (positive_value == 'positive' and  confidences_value < 0.6):
-                    parts.append('⭐️⭐️⭐️⭐️')
+                    parts.append('4')
                 elif positive_value == 'neutral':
-                    parts.append('⭐️⭐️⭐️')
+                    parts.append('3')
                 elif (positive_value == 'negative'and confidences_value < 0.6):
-                    parts.append('⭐️⭐️')
+                    parts.append('2')
                 elif (positive_value == 'negative'and confidences_value >= 0.6):
-                    parts.append('⭐️')
+                    parts.append('1')
                 absa_pred[i] = ','.join(parts)
             results.append(absa_pred)
         else:
@@ -119,9 +105,30 @@ def show_predict_csv(df_clean, output_csv_path):
     df_input_with_pred = df_source.copy()
     df_input_with_pred['label'] = results
     df_input_with_pred.to_csv(output_csv_path, index=False)
+    
+# final_output_with star file csv
+def extract_first_last(text):
+    parts = text.split(',')
+    first_value = parts[0].split("'")[1]
+    last_value = parts[-1].split("'")[0]
+    return first_value, last_value
 
+# Function to add icon to average star values
+def add_icon_to_avg_star(row):
+    avg_star = row['star']
+    icon = '⭐'  # Star icon
+    return f"{avg_star}{icon}"
 
-# datavao = "data_user/test.csv"
-# df_clean = pd.read_csv(datavao, index_col=None)
-# datara = "data_user/chuachuan.csv"
-# show_predict_csv(df_clean,datara)
+def show_predict_csv():
+    df = pd.read_csv("./data_user/data_with_label.csv")
+    df = df.dropna()
+    df[['aspect', 'star']] = df['label'].apply(lambda x: pd.Series(extract_first_last(x)))
+    
+    drop_col = df.columns[0:2]
+    df.drop(columns=drop_col, axis=1)
+    
+    df['star'] = df['star'].astype('int') 
+    avg_by_aspect = df.groupby('aspect')['star'].mean().round(1)
+    result_df = avg_by_aspect.reset_index(name='star')
+    result_df['star'] = result_df.apply(add_icon_to_avg_star, axis=1)
+    return result_df
