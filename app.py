8b91460564c83e55f5703df35a928f525fb71997
Vũ Thị Linh <vuthilinh.jvb@gmail.com>
2024-04-16 15:39:03 +0700

    fix: input usern

--- app.py
+++ app.py
@@ -5,20 +5,12 @@ from streamlit_lottie import st_lottie
 import time
 import pandas as pd
 import os
-from utils.preprocess_user_data import auto_detect_filter_data, keep_longest_average_columns
+from utils.preprocess_user_data import auto_detect_filter_data
 from utils.preprocess_user_data import preprocess_data
 from utils.tokenizer import tokenize_function, call_tokenizer
 from utils.preprocess_text import preprocess
-# from predict import show_predict_text,show_predict_csv
-from predict import show_predict_csv, show_predict_text
+from predict import show_predict_text,process_predict_csv, show_predict_csv
 from annotated_text import annotated_text
-
-
-
-st.set_page_config(
-    page_title="ML APP",
-    page_icon="👋",
-)
 
 # Initialize session state for file upload status
 if 'file_uploaded' not in st.session_state:
@@ -43,7 +35,7 @@ if choice == 'Home':
     st.title("こんにちは! Welcome to our ABSA web app😊")
     st_lottie(lottie_robot, speed=1, loop=True, quality="low")
     # snowfall
-    if st.button("きれいなゆき・Bông tuyết trong sạch]🤡"):
+    if st.button("きれいなゆき・Bông tuyết trong sạch🤡"):
         st.snow()
 
 elif choice == 'Upload':
@@ -56,7 +48,6 @@ elif choice == 'Upload':
 
     # Nếu trạng thái sẵn sàng nhập là True, hiển thị ô nhập văn bản
     if st.session_state['ready_to_input']:
-        
         user_input = st.text_input("Enter some review about your smartphone 👇", key='user_input',placeholder="This is a placeholder...")
 
         # Nếu người dùng nhấn Enter trong ô nhập liệu (text_input luôn trả về giá trị, kể cả chuỗi rỗng)
@@ -72,36 +63,42 @@ elif choice == 'Upload':
             st.warning('Please ensure to fill some text before hitting enter.')  # Cảnh báo nếu không nhập gì
     st.title("Upload your data here")
     file = st.file_uploader("We accept various types of data. So don't worry, just go ahead!")
-    print(file,"file")
+    # if file:
+    #     df = pd.read_csv(file, index_col=None)
+    #     df.to_csv('data_user/source.csv', index=None)
+    #     st.dataframe(df,use_container_width=True)
+    #     st.success("Yahoo! Your data has been uploaded successfully. Now move to the next step for preprocessing🎉",)
+    #     st.session_state.file_uploaded = True
     if file:
-      
-        file_extension = file.name.split(".")[-1]
-        if file_extension in ["csv", "json"]:
-            df = pd.read_csv(file, index_col=None)
-        elif file_extension in ["xlsx", "xls", "xlsm"]:
-            df = pd.read_excel(file, index_col=None)
-        df.to_csv('data_user/source.csv', index=None, encoding='utf-8')
-        st.dataframe(df)
-        st.success("Yahoo! Your data has been uploaded successfully. Now move to the next step for preprocessing🎉",)
-        st.session_state.file_uploaded = True   
+         file_extension = file.name.split(".")[-1]
+    if file_extension in ["csv", "json"]:
+        df = pd.read_csv(file, index_col=None)
+    elif file_extension in ["xlsx", "xls", "xlsm"]:
+        df = pd.read_excel(file, index_col=None)
+    df.to_csv('data_user/source.csv', index=None, encoding='utf-8')
+    st.dataframe(df)
+    st.success("Yahoo! Your data has been uploaded successfully. Now move to the next step for preprocessing🎉",)
+    st.session_state.file_uploaded = True  
+        
 elif choice in ['Apply ABSA']:
     if not st.session_state.file_uploaded:
         st.warning("Please upload a file first before proceeding to this step.")
     else:       
         if choice == "Apply ABSA":
             lottie_data_to_ai = load_lottiefile("lottiefiles/data_to_ai.json")
-            st_lottie(lottie_data_to_ai, speed=1, loop=True, quality="low")
+            st_lottie(lottie_data_to_ai, speed=1, loop=True, quality="low")    
             input_path = "data_user/source.csv"
             output_path = "data_user/raw.csv"
-            keep_longest_average_columns(input_path, output_path)
             auto_detect_filter_data(input_path, output_path)
             df_detect = pd.read_csv(output_path, index_col=None)
             df_clean = preprocess_data(df_detect)
             output_csv_path = "data_user/data_with_label.csv"  # Specify output CSV file path
-            show_predict_csv(df_clean, output_csv_path)
-            read = pd.read_csv(output_csv_path)
-            st.dataframe(read)
-
+            process_predict_csv(df_clean, output_csv_path)
+            df = pd.read_csv(output_csv_path)
+            show = show_predict_csv()
+            st.dataframe(show)
+            if st.button('Click here if you want know more details🫶'):
+                st.dataframe(df)
 elif choice == 'About us':
     st.markdown("<h1 style='text-align: center; color: black;'>About Us</h1>", unsafe_allow_html=True)
     url_company = "https://jvb-corp.com/vi/"
@@ -112,7 +109,7 @@ elif choice == 'About us':
         st.markdown("<h2 style='color: black;'>🤝Our Organization🤝</h2>", unsafe_allow_html=True)
         annotated_text(
             "Hi, I'm",
-            ("JVB-Junior", "", "#faa"),
+            ("Junior-VB", "", "#faa"),
             "🤖"
         )
         st.markdown("""- I was created by a team of AI interns from JVB Vietnam company.\n - Beside ABSA model, we also provide other technology solutions.\n - Check out this [link](%s) for more information about our group""" % url_company)
@@ -141,4 +138,4 @@ elif choice == 'About us':
         lottie_col1 = load_lottiefile("lottiefiles/github.json")
         st_lottie(lottie_col1, speed=1, loop=True, quality="low")
         
-    st.markdown("<h4 style='text-align: center; color: black; opacity: 0.5;'>ありがとう ございます</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='text-align: center; color: black; opacity: 0.5;'>ありがとう ございます。</h4>", unsafe_allow_html=True)
