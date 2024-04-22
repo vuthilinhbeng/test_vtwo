aba47e016735db43ee05df8406095e48c258fe82
linhgau <gaudauthui.com>
2024-04-22 15:30:24 +0700

    update

--- predict.py
+++ predict.py
@@ -25,7 +25,7 @@ def load_model():
         reloaded_model.load_weights('model.h5')
         return reloaded_model
 
-# reloaded_model = load_model()
+reloaded_model = load_model()
 
 # pretrained_bert = TFAutoModel.from_pretrained(PRETRAINED_MODEL, output_hidden_states=True)
 # reloaded_model = create_model(pretrained_bert)
